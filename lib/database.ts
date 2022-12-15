e2ee5966287d66b4e8524a1f13003b0b95f5b442
monkeyts <monkeytypescript@gmail.com>
2022-12-15 17:25:36 -0600

    Convert enivonrment variable, port, to number

--- lib/database.ts
+++ lib/database.ts
@@ -1,8 +1,8 @@
 import postgres from "postgres";
 
-const sql = postgres("postgres://username:password@host:port/database", {
+const sql = postgres("postgres://", {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
