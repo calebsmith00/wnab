82b2034a2fa3ed1e135f58a684ca3914bdc6e8fa
monkeyts <monkeytypescript@gmail.com>
2022-12-24 00:35:29 -0600

    Update input components to accept UI changes

--- components/Input/Group.tsx
+++ components/Input/Group.tsx
@@ -1,3 +1,4 @@
+import { ChangeEventHandler } from "react";
 import InputItem from "./Item";
 import InputLabel from "./Label";
 
@@ -5,17 +6,26 @@ interface InputGroupProps {
   for?: string;
   type?: string;
   text?: string;
+  handleChange?: ChangeEventHandler<HTMLInputElement>;
+  required?: boolean;
+  value?: string;
   children?: JSX.Element[];
 }
 
 const defaultStyle: string = "grid place-content-center gap-y-3 mt-5";
 
 export default function InputGroup(props: InputGroupProps) {
-  if (props.for && props.text && !props.children) {
+  if (props.for && props.text && props.handleChange && !props.children) {
     return (
       <div className={defaultStyle}>
         <InputLabel for={props.for} text={props.text} />
-        <InputItem name={props.for} type={props.type} />
+        <InputItem
+          name={props.for}
+          type={props.type}
+          handleChange={props.handleChange}
+          required={props.required}
+          value={props.value}
+        />
       </div>
     );
   } else {
--- components/Input/Item.tsx
+++ components/Input/Item.tsx
@@ -1,10 +1,23 @@
+import { ChangeEventHandler } from "react";
+
 interface InputItemProps {
   type?: string;
+  required?: boolean;
+  value?: string;
+  handleChange?: ChangeEventHandler<HTMLInputElement>;
   name: string;
 }
 
 export default function InputItem(props: InputItemProps) {
   return (
-    <input id={props.name} name={props.name} type={props.type || "text"} />
+    <input
+      id={props.name}
+      name={props.name}
+      type={props.type || "text"}
+      className="text-black py-1 mx-3 px-2"
+      onChange={props.handleChange}
+      required={props.required || false}
+      value={props.value}
+    />
   );
 }
