8eedf41832e682e0ffae15286cbc937094ab2ded
monkeyts <monkeytypescript@gmail.com>
2022-12-30 16:34:19 -0600

    Update min height and width on BudgetCard

--- components/Budget/Card.tsx
+++ components/Budget/Card.tsx
@@ -4,7 +4,7 @@ interface BudgetCardProps {
 
 export default function BudgetCard(props: BudgetCardProps) {
   return (
-    <div className="bg-gray-500/50 rounded-xl p-10 m-5 min-w-[200px]">
+    <div className="bg-gray-500/50 rounded-xl p-10 m-5 min-w-[1.5em] min-h-[1em]">
       <h1>{props.title}</h1>
     </div>
   );
