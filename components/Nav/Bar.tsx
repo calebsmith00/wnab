7509d083f3342e7f3ada14c0679886d76c5ea536
monkeyts <monkeytypescript@gmail.com>
2022-12-29 19:32:07 -0600

    Add link to view budgets

--- components/Nav/Bar.tsx
+++ components/Nav/Bar.tsx
@@ -21,10 +21,11 @@ export default function Navbar() {
   return (
     <div className="grid justify-items-end grid-rows-auto grid-cols-2 items-center text-2xl">
       <div className="pl-5 pt-3 justify-self-start">Logo</div>
-      <div className={`pr-5 pt-3 grid lg:grid-cols-3`}>
+      <div className="pr-5 pt-3 grid lg:grid-cols-4">
         <NavLink link="/" label="Home" isMenuOpen={isMenuOpen} />
         <NavLink link="/login" label="Login" isMenuOpen={isMenuOpen} />
         <NavLink link="/register" label="Register" isMenuOpen={isMenuOpen} />
+        <NavLink link="/budgets" label="Budgets" isMenuOpen={isMenuOpen} />
         <div onClick={handleMenu} className={`lg:hidden cursor-pointer`}>
           {(isMenuOpen && "Close") || "Open"}
         </div>
