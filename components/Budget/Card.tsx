c9da9f18689fff99e75349bbd6d6d70e9ff94d81
monkeyts <monkeytypescript@gmail.com>
2023-01-11 12:37:29 -0600

    Add ability to select a budget from list of budgets

--- components/Budget/Card.tsx
+++ components/Budget/Card.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 interface BudgetCardProps {
   title: string;
+  id: number;
 }
 
 export default function BudgetCard(props: BudgetCardProps) {
+  const router = useRouter();
+
+  const accessBudget = () => {
+    router.push(`/budgets/${props.id}`);
+  };
+
   return (
-    <div className="bg-gray-500/50 rounded-xl p-10 m-5 min-w-[1.5em] min-h-[1em]">
+    <div
+      className="bg-gray-500/50 rounded-xl p-10 m-5 min-w-[1.5em] min-h-[1em] cursor-pointer"
+      onClick={accessBudget}
+    >
       <h1>{props.title}</h1>
     </div>
   );
